5c7315e930e5964c61d4e83f2c32392ecf9d4890
d.preti <d.preti@almawave.it>
2023-02-17 11:00:19 +0100

    fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='stat_analysis',
-   version='1.0',
+   version='1.0.1',
    description='scientific stat tools',
    author='David Preti',
    license = "MIT",
